d34ed191300a40a934f818910fd7d4d55f14b6a7
Leon Prouger <leonprou@gmail.com>
2019-01-03 17:15:11 +0200

    adding events

--- contracts/SimpleList.sol
+++ contracts/SimpleList.sol
@@ -13,6 +13,12 @@ contract SimpleList is Ownable {
   mapping(string => uint) private _entityMap;
   string[] private _entityList;
 
+  event EntityAdded(string hash);
+  event EntityDeleted(string hash);
+  event AdminAdded(address _address);
+  event AdminRemoved(address _address);
+
+
   modifier onlyAdmin() {
     require (admins[msg.sender] == 1);
     _;
@@ -24,6 +30,7 @@ contract SimpleList is Ownable {
 
   function addEntity(string hash) public onlyAdmin returns(bool) {
     _entityMap[hash] = _entityList.push(hash) - 1;
+    emit EntityAdded(hash);
     return true;
   }
 
@@ -37,18 +44,21 @@ contract SimpleList is Ownable {
     _entityList[rowToDelete] = keyToMove;
     _entityMap[keyToMove] = rowToDelete;
     _entityList.length--;
+    emit EntityDeleted(hash);
     return true;
   }
 
   function addAdmin(address _address) public onlyOwner returns(bool) {
     require(_address != address(0));
     admins[_address] = 1;
+    emit AdminAdded(_address);
     return true;
   }
 
   function removeAdmin(address _address) public onlyOwner returns(bool) {
     require(_address != address(0));
     delete admins[_address];
+    emit AdminRemoved(_address);
     return true;
   }
 
