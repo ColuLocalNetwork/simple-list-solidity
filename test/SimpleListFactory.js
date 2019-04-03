507bcfc83be75ee42feea0b805f20a9ef2b637ee
Leon Prouger <leonprou@gmail.com>
2019-04-03 16:35:10 +0300

    add token address to emitted event

--- test/SimpleListFactory.js
+++ test/SimpleListFactory.js
@@ -11,12 +11,12 @@ contract("SimpleListFactory", accounts => {
     })
 
     it('should create simple list', async () => {
-      const tokenAddress = '0x14456a52832891c3dcf4f7529949817cb3048cc0'
+      const tokenAddress = '0x14456a52832891c3dCf4f7529949817cB3048CC0'
       let listAddress
       const result = await factory.createSimpleList(tokenAddress)
       truffleAssert.eventEmitted(result, 'SimpleListCreated', (ev) => {
         listAddress = ev.list
-        return ev.admin === admin
+        return ev.admin === admin && ev.token === tokenAddress
       })
       assert.equal(await factory.tokenToListMap(tokenAddress), listAddress)
     })
